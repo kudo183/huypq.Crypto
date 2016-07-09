45011cc2b32bca1f0e7e6cb0a36f3180dcde4847
kudo183 <kudo183@gmail.com>
2016-07-09 11:46:03 +0700

    - initialize Crypto project.

---
+++ src/huypq.Crypto.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace huypq.Crypto.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new PasswordHash();
+            var r = p.HashedBase64String("this is a password");
+            var resultTrue = PasswordHash.VerifyHashedPassword(r, "this is a password");
+            var resultFalse = PasswordHash.VerifyHashedPassword(r, "this is another password");
+        }
+    }
+}
